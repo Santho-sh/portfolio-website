d0d827e02c84a945f1c3e35f21695048092f304f
Santhosh <santhoshkalish333@gmail.com>
2023-01-05 21:23:27 +0530

    before change

--- js/main.js
+++ js/main.js
@@ -2,14 +2,17 @@ const primaryNav = document.querySelector('.nav__links');
 const navToggle = document.querySelector('.menu__toggle');
 
 
-    navToggle.addEventListener("click", () => {
-        const visibile = primaryNav.getAttribute('data-visible');
+navToggle.addEventListener("click", () => {
+  const visibile = primaryNav.getAttribute('data-visible');
 
-        if (visibile == 'false') {
-          primaryNav.setAttribute('data-visible', true);
-        }
-        else if (visibile == 'true') {
-          primaryNav.setAttribute('data-visible', false);
-        }
-    });
+  if (visibile == 'false') {
+    primaryNav.setAttribute('data-visible', true);
+  }
+  else if (visibile == 'true') {
+    primaryNav.setAttribute('data-visible', false);
+  }
+});
 
+function myFunction(x) {
+  x.classList.toggle("change");
+}
